4ae2c33c11cc77cd6c928a5710e9c3760e4f1288
SudhanshuPrasad152 <104454913+SudhanshuPrasad152@users.noreply.github.com>
2023-09-04 19:23:01 +0530

    fourth commit

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ connectToMongo();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(express.json());
 app.use(cors());
